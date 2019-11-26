633ec0ce6be64c737731c28ee405769bda3927d8
Paulo Henrique <phenriq694@gmail.com>
2019-11-26 18:57:44 -0300

    newPost styles.

--- frontend/src/pages/Feed/Components/NewPost.js
+++ frontend/src/pages/Feed/Components/NewPost.js
@@ -2,19 +2,12 @@ import React from 'react';
 
 function NewPost() {
   return (
-<<<<<<< HEAD
-    <div className="newPost">
-      <input />
-      <button type="button">Postar</button>
-    </div>
-=======
     <>
-      <div className="newPost" style={{maxWidth: "100%"}}>
+      <div className="newPost">
         <input />
         <button type="button">Postar</button>
       </div>
     </>
->>>>>>> 866a639c0c190f2d815ce4b19efbc9f6c3648382
   );
 }
 
