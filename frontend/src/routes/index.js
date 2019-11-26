e1ba61428e183632d3c397b9a440abed1f21814e
Lucas A. Paixao <lucas@unimedvaledoaco.coop.br>
2019-11-26 16:11:55 -0300

    Front instituição

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -6,14 +6,16 @@ import Route from './Route';
 import Home from '../pages/Home';
 import Perfil from '../pages/Perfil';
 import Projetos from '../pages/Projetos';
+import Instituicao from '../pages/Instituicao';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact to='/home'/>
-      <Route path="/home" component={Home} isPrivate/>
-      <Route path="/perfil" component={Perfil} isPrivate/>
-      <Route path="/projetos" component={Projetos} isPrivate/>
+      <Route path="/" exact to="/home" />
+      <Route path="/home" component={Home} isPrivate />
+      <Route path="/perfil" component={Perfil} isPrivate />
+      <Route path="/projetos" component={Projetos} isPrivate />
+      <Route path="/Instituicao" component={Instituicao} isPrivate />
     </Switch>
   );
 }
