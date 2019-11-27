47ac7170a357bdbca2a062d5fd8d765a875faa74
Paulo Henrique <phenriq694@gmail.com>
2019-11-27 12:30:07 -0300

    adding config in 'Postar' button.

--- frontend/src/pages/Feed/Components/PostList.js
+++ frontend/src/pages/Feed/Components/PostList.js
@@ -1,57 +1,27 @@
 import React, { Component } from 'react'
 
 import PostItem from './PostItem'
-import avatar from '../../../assets/user.svg'
 
 class PostList extends Component {
 
   state = {
     posts: [
-      {
-        id: 1,
-        author: {
-          name: 'Instituição 1',
-          avatar: avatar
-        },
-        date: '04 Jun 2019',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae dictum ipsum, eu elementum orci. Vivamus ligula sem, consectetur a dolor ut, semper rhoncus massa. Duis sit amet accumsan ipsum. Ut quis gravida risus. Quisque congue molestie pulvinar. Donec ultricies est eu lobortis interdum. Nam maximus scelerisque diam, nec rutrum nulla rutrum vitae. Vestibulum ac tellus nec dui lobortis pretium. Nulla commodo libero lacus, at dignissim dui pharetra in. Aliquam ac quam non lorem sodales varius. Nunc fringilla tempor leo at tincidunt. Praesent dictum, felis in mollis condimentum, nisi turpis aliquet elit, eu blandit lorem dui a massa. Vestibulum quis neque tortor. Fusce sit amet vestibulum leo. Phasellus lorem ligula, molestie et pharetra at, bibendum sit amet turpis. Donec eget turpis at massa consequat placerat at sed justo.',
-        comments: [
 
-        ]
-      },
-      {
-        id: 3,
-        author: {
-          name: 'Neil Cook',
-          avatar: avatar
-        },
-        date: '04 Jun 2019',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae dictum ipsum, eu elementum orci. Vivamus ligula sem, consectetur a dolor ut, semper rhoncus massa. Duis sit amet accumsan ipsum. Ut quis gravida risus. Quisque congue molestie pulvinar. Donec ultricies est eu lobortis interdum. Nam maximus scelerisque diam, nec rutrum nulla rutrum vitae. Vestibulum ac tellus nec dui lobortis pretium. Nulla commodo libero lacus, at dignissim dui pharetra in. Aliquam ac quam non lorem sodales varius. Nunc fringilla tempor leo at tincidunt. Praesent dictum, felis in mollis condimentum, nisi turpis aliquet elit, eu blandit lorem dui a massa. Vestibulum quis neque tortor. Fusce sit amet vestibulum leo. Phasellus lorem ligula, molestie et pharetra at, bibendum sit amet turpis. Donec eget turpis at massa consequat placerat at sed justo.',
-        comments: [
-
-        ]
-      },
-      {
-        id: 6,
-        author: {
-          name: 'Gabriel Lisboa',
-          avatar: avatar
-        },
-        date: '04 Jun 2019',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae dictum ipsum, eu elementum orci. Vivamus ligula sem, consectetur a dolor ut, semper rhoncus massa. Duis sit amet accumsan ipsum. Ut quis gravida risus. Quisque congue molestie pulvinar. Donec ultricies est eu lobortis interdum. Nam maximus scelerisque diam, nec rutrum nulla rutrum vitae. Vestibulum ac tellus nec dui lobortis pretium. Nulla commodo libero lacus, at dignissim dui pharetra in. Aliquam ac quam non lorem sodales varius. Nunc fringilla tempor leo at tincidunt. Praesent dictum, felis in mollis condimentum, nisi turpis aliquet elit, eu blandit lorem dui a massa. Vestibulum quis neque tortor. Fusce sit amet vestibulum leo. Phasellus lorem ligula, molestie et pharetra at, bibendum sit amet turpis. Donec eget turpis at massa consequat placerat at sed justo.',
-
-        comments: [
-          
-        ]
-      }
     ]
+  }
+
+  componentDidMount() {
+    const posts = localStorage.getItem('posts')
+
+    if(posts) {
+      this.setState({posts: JSON.parse(posts)})
+    }
   }
 
 
   render() {
     return (
-      <div className="postList">
-        
+      <div className="postList">   
         {this.state.posts.map(post => <PostItem key={post.id} {...post} />)}
       </div>
     )
